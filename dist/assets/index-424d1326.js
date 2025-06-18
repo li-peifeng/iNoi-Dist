da10c774a83784672cfe025d42e1c4bfe5fd772b
iSweeTiamo <103113557+li-peifeng@users.noreply.github.com>
2025-06-18 21:37:08 +0200

    upload dev dist

---
+++ dist/assets/index-424d1326.js
@@ -0,0 +1,1 @@
+import{y as t,ag as e,aa as r}from"./index-f8d8703a.js";import n from"./Upload-b6f732d3.js";import"./index-5ef86c96.js";const c=()=>t(r,{justifyContent:"center",h:"100vh",get children(){return t(e,{w:"$md",get children(){return t(n,{})}})}});export{c as default};
