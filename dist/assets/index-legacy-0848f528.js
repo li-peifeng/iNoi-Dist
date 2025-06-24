f7d3c977b7729d76e0cad2b02dd297840e4a06e1
LEO <inoi@peifeng.li>
2025-06-24 18:48:57 +0000

    upload 2025.06.25 dist files

--- dist/assets/index-legacy-0848f528.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-a72ba3a2.js","./Upload-legacy-722095f2.js","./index-legacy-33a8f1aa.js"],function(e,t){"use strict";var n,r,u,a;return{setters:[e=>{n=e.y,r=e.ag,u=e.aa},e=>{a=e.default},null],execute:function(){e("default",()=>n(u,{justifyContent:"center",h:"100vh",get children(){return n(r,{w:"$md",get children(){return n(a,{})}})}}))}}});
