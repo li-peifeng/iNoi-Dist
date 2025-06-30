009386606cb6047746a6161fee388ddbf5f51bbd
LEO <inoi@peifeng.li>
2025-06-30 14:17:16 +0000

    upload 2025.06.30 dist files

--- dist/assets/index-legacy-fa539fbe.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-5697e769.js","./Upload-legacy-3dd5a168.js","./index-legacy-de8f258a.js"],function(e,t){"use strict";var n,r,u,a;return{setters:[e=>{n=e.y,r=e.ag,u=e.aa},e=>{a=e.default},null],execute:function(){e("default",()=>n(u,{justifyContent:"center",h:"100vh",get children(){return n(r,{w:"$md",get children(){return n(a,{})}})}}))}}});
